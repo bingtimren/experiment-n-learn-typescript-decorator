5040aab12fc2f123dc82b62daf22b75a9d7cfd27
Bing Ren <bingtimren@gmail.com>
2019-06-14 00:01:43 +1000

    Completed class, method, accessor decorators

---
+++ src/lib/class-decorator.spec.ts
@@ -0,0 +1,124 @@
+// tslint:disable
+import test from 'ava';
+import assert from 'assert'
+
+/**
+ * This test is a demo of class decorators in typescript (experimental).
+ * 
+ * See: https://www.typescriptlang.org/docs/handbook/decorators.html
+ * 
+ */
+
+/*************************************************************************************************************************
+ * This section demonstrates:
+ * 
+ * (1) Class decorator receives a class (in Javascript, the constructor function) as target.
+ * (2) A class decorator's ability (and syntax) to dynamically extend the target class.
+ **************************************************************************************************************************/
+
+/**
+ * This is the decorator. It's a function that receives a class target (a constructor function). It dynamically extends
+ * the target.
+ *  
+ * @param target - the class to be extended
+*/
+
+function classDecoratorThatExtends<T extends new (...args: any[]) => {}>(target: T) {
+    // Here we examine the target, which should be the Greeter
+    // So, a class decorator receives the decorated class, which in JS is just the constructor function, as the target
+    assert(target.name === 'Greeter')
+    assert(target.toString().startsWith("class Greeter {"))
+    // here we dynamically extends the target, add some new property and methods
+    return class ExtendedTarget extends target {
+        public newProperty = 'new property';
+        public newMethod() {
+            return 'new method';
+        }
+    };
+}
+
+/**
+ * Here looks like the "old" Greeter class but with the decoration, it's in fact a syntax sugar of
+ * decorating (calling decorator) the old Greeter class and resulting a new, extended class.
+ */
+@classDecoratorThatExtends
+class Greeter {
+    public property = 'old property';
+    constructor(public hello: string) { }
+    public method() {
+        return 'old method:' + this.hello;
+    }
+}
+
+/**
+ * This demonstrates that the classDecoratorThatExtends indeed extends the old Greeter class, so the 
+ * variable 'Greeter' no longer refers to the old Greeter class but the extended class that is returned
+ * from the decorator
+ */
+test('Class decorator that dynamically extends Greeter', t => {
+    // this demos that Greeter after decoration is the extended class
+    t.is(Greeter.name, 'ExtendedTarget');
+    // so the instance g is instance of an extended class
+    const g = new Greeter('Winston') as any;
+    // examines the instance g has all the old and new properties and methods
+    t.is(g instanceof Greeter, true);
+    t.is(g.property, 'old property');
+    t.is(g.newProperty, 'new property')
+    t.is(g.hello, 'Winston');
+    t.is(g.method(), 'old method:Winston');
+    t.is(g.newMethod(), 'new method');
+});
+
+/*************************************************************************************************************************
+ * This section demonstrates that class decorator can return a class that does not extend the target, but nevertheless
+ * Typescript still checks the compatibility of the returned class with the target
+ **************************************************************************************************************************/
+
+/**
+ * This is class "Foo" that will be returned by the class decorator classDecoratorThatReplaces to replace class "Bar"
+ * Typescript would still check the signature of the return object of the class decorator for compatibility with the 
+ * decorated class, Bar. 
+ * 
+ * Since Bar has a public property 'bar', here Foo must also has that property to be compatible with Bar, though Foo does not 
+ * necessarily extends Bar.
+ */
+
+class Foo {
+    public foo:boolean;
+    public bar:boolean = true; // this is important, otherwise Typescript reports error
+    constructor(){
+        this.foo = true
+    }
+}
+
+/**
+ * This is a class decorator that replaces Bar with Foo. Although Foo does not need to extend Bar, it still needs to be compatible with Bar.
+ * @param target - to receive Bar in the test
+ */
+
+function classDecoratorThatReplaces<T extends new (...args: any[]) => {}>(_target: T) {
+    return Foo;
+}
+
+/**
+ * This is the decorated Bar class. After decoration, variable 'Bar' actually refers to the class 'Foo'
+ */
+@classDecoratorThatReplaces
+class Bar {
+    public bar:boolean;
+    constructor(){
+        this.bar = true
+    }
+}
+
+/**
+ * This demonstrates that the classDecoratorThatExtends indeed extends the old Greeter class, so the 
+ * variable 'Greeter' no longer refers to the old Greeter class but the extended class that is returned
+ * from the decorator
+ */
+test('Class decorator that replaces Bar with Foo', t => {
+    // Bar is indeed replaced with Foo
+    t.is(Foo === Bar, true);
+});
+
+
---
+++ src/lib/method-accessor-decorator.spec.ts
@@ -0,0 +1,149 @@
+// tslint:disable
+import test from 'ava';
+import assert from 'assert'
+
+/*******************************************************************************************************************
+ * This section demonstrates:
+ * 
+ * - Instance method decorator is a function that receives:
+ *      - target: the prototype object of the class (of which the instance method is a property)
+ *      - propertyKey: string, name (string key) of the method
+ *      - descriptor: the property descriptor, with 'value' being the method function
+ * 
+ * - Static method decorator is a function that receives:
+ *      - target: the class (of which the static method is a property)
+ *      - propertyKey: string, name (string key) of the method
+ *      - descriptor: the property descriptor, with 'value' being the method function
+ * 
+ * - Getter / setter decorator is a function that receives:
+ *      - target: the prototype object of the class (of which the getter/setter is a property)
+ *      - propertyKey: string, name (string key) of the property
+ *      - descriptor: the property descriptor, with 'get'/'set' being the getter/setter function
+ * 
+ * - Decorator's ability to modify the method by returning a new descriptor
+ * 
+ * - How to wrap around the original method to modify its behaviour
+ * 
+ *******************************************************************************************************************/
+
+ /**
+  * This is a decorator for an instance method (not a static method)
+  * @param target - the class's prototype
+  * @param propertyKey - name of the method, also the string key of the method as property of the prototype object
+  * @param descriptor - the property descriptor
+  */
+
+
+function instanceMethodDecorator(target: any, propertyKey: string, descriptor: PropertyDescriptor) : PropertyDescriptor{
+    // asserts the received arguments are as described
+    assert(target === Greeter.prototype)
+    assert(propertyKey === 'greet')
+    assert(target[propertyKey] === descriptor.value)
+
+    // below demonstrates how to wrap around the original method to modify its behaviour
+    const originalMethod = target[propertyKey] as Function
+    // return a descriptor that replaces the 'value' with a new function that wraps around the old function
+    return Object.assign({}, descriptor, {
+        value: function(...args:any[]) {
+            // as an instance method, expects 'this' to be an instance of the class
+            assert(this instanceof Greeter)
+            return originalMethod.apply(this, args)+"!!!"
+        }
+    }) 
+}
+
+/**
+ * Static method decorator
+ * @param target - the constructor function, i.e. the class
+ * @param propertyKey - name of the method
+ * @param descriptor - property descriptor
+ */
+function staticMethodDecorator(target: any, propertyKey: string, descriptor: PropertyDescriptor) : PropertyDescriptor{
+    // asserts the received arguments are as described
+    assert(target === Greeter)
+    assert(propertyKey === 'staticGreet')
+    assert(target[propertyKey] === descriptor.value)
+
+    // below demonstrates how to wrap around the original method to modify its behaviour
+    const originalMethod = target[propertyKey] as Function
+    // return a descriptor that replaces the 'value' with a new function that wraps around the old function
+    return Object.assign({}, descriptor, {
+        value: function(...args:any[]) {
+            // as a static method, 'this' is the class
+            assert(this as any === Greeter)
+            return originalMethod.apply(this, args)+"..."
+        }
+    }) 
+}
+
+/**
+ * Accessor decorator 
+ * @param target - the class's prototype
+ * @param propertyKey - the property key, i.e. the name of the property
+ * @param descriptor - the property descriptor
+ */
+function accessorDecorator(target: any, propertyKey: string, descriptor: PropertyDescriptor) : PropertyDescriptor{
+    // asserts the received arguments are as described
+    assert(target === Greeter.prototype)
+    assert(propertyKey === 'message')
+    const greeterMessagePropertyDescriptor = Reflect.getOwnPropertyDescriptor(target, propertyKey) as PropertyDescriptor
+    assert(greeterMessagePropertyDescriptor.get === descriptor.get && greeterMessagePropertyDescriptor.set === descriptor.set)
+
+    // below demonstrates how to wrap around the original getter & setter to modify its behaviour
+    const originalGetter = greeterMessagePropertyDescriptor.get as Function
+    const originalSetter = greeterMessagePropertyDescriptor.set as Function
+    // return a descriptor that replaces both the getter and setter with new functions that wrap around the old functions
+    return Object.assign({}, descriptor, {
+        get: function() {
+            assert(this instanceof Greeter)
+            return "{"+originalGetter.apply(this)+"}"
+        },
+        set: function(message:string) {
+            assert(this instanceof Greeter)
+            originalSetter.apply(this, ["*"+message+"*"])
+        }
+    }) 
+}
+
+
+/**
+ * The class that has decorated static and instance methods
+ */
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+
+    @instanceMethodDecorator
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+    @staticMethodDecorator
+    static staticGreet() {
+        return "Hi there"
+    }
+    @accessorDecorator
+    get message():string {return this.greeting};
+    // it would be wrong to also decorate the setter - only one of the getter / setter of same name can be decorated
+    set message(message:string){ this.greeting = message}
+}
+
+
+// tests that the decorators actually works
+
+test('Instance method decorator works', t => {
+    const g = new Greeter("human")
+    t.is(g.greet(), "Hello, human!!!")
+})
+
+test('Static method decorator works', t => {
+    t.is(Greeter.staticGreet(), "Hi there...")
+})
+
+test('Getter & Setter decorator works', t => {
+    const g = new Greeter("mate")
+    t.is(g.message, "{mate}")
+    g.message = "boy"
+    t.is(g.message, "{*boy*}")
+})
