9428bf90cddaab7604b88b9f1bedaa85419b1787
Bing Ren <bingtimren@gmail.com>
2019-06-22 11:11:29 +1000

    Added test to sub class of parameter decorated class

--- src/parameter-decorator.spec.ts
+++ src/parameter-decorator.spec.ts
@@ -107,14 +107,19 @@ class Greeter {
     }
 }
 
-// // test inheritence behavior
-// class ExtendedDecoratedGreeter extends Greeter{
-// }
+// test inheritence behavior
+class ExtendedDecoratedGreeter extends Greeter{
+}
 
 
 test('Property decorator', t => {
+    // test the decorator and the parameter interception works
     t.is(Greeter.staticSayHi("Roger", "pie day"), "Hi Roger!, happy pie day!")
     const g = new Greeter("Bob")
     t.is(g.instanceSayHi("Roger","pie day"), "Bob says: Hi Roger!, happy pie day!")
+    // test the sub class inheritance works as expected
+    t.is(ExtendedDecoratedGreeter.staticSayHi("Roger", "pie day"), "Hi Roger!, happy pie day!")
+    const eg = new ExtendedDecoratedGreeter("Bob")
+    t.is(eg.instanceSayHi("Roger","pie day"), "Bob says: Hi Roger!, happy pie day!")
     
 })
